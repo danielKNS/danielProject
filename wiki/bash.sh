f5a7a808d39faf9ca692d5441a93ecc1eab83055
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-03-19 15:24:19 -0400

    new update

--- wiki/bash.sh
+++ wiki/bash.sh
@@ -3,3 +3,19 @@ pwd # this means present working director ( where you are in the computer)
 cd # change directory (it takes you to the folder)
 
 touch # crete a f
+
+#  BASH  BASIC :
+
+mkdir	# create directory
+cd    # change directory, go to that folder
+cd .. # go back one directory
+cp    # copy file. (Ex: `cp file1.txt file2.txt`)
+ls    # list everything inside your current folder
+ls *.py # shows all files with .py in the end
+rm    # remove file
+pwd	  # print working directory (show where you are right now)
+mv    # rename file
+clear # clears window
+./filename # run file (must be executable)
+man   # open manual
+man some_command # open that manual
