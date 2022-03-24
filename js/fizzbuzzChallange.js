34bb5b8a101e4680ccbe6d4e4edecbdd716cb5e7
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-03-24 14:52:50 -0400

    new update

--- js/fizzbuzzChallange.js
+++ js/fizzbuzzChallange.js
@@ -8,13 +8,22 @@ whenever a number is divisble by both 3 and 5, we want to print out the word "Fi
 
 REMEMBER THAT DIVISIBLE IS A NUMBER THAT IS CAPABLE TO BE DIVIDED.
 */
-
+console.log (" -------for loop --------")
 const fizzBuzz = (numbers) => {
- for (let n = 1; n <= numbers ; n++ )
+ for (let n = 1; n <= numbers ; n++ ){
  if (n % 3 === 0 && n % 5 === 0) console.log ("FizzBuzz")
  else if (n % 3 === 0) console.log ("Fizz")
  else if (n % 5 === 0) console.log ("Buzz")
  else console.log (n)
-}
+}}
 
 console.log(fizzBuzz(100))
+
+console.log ("-----forEach loop------")
+const fizzBuzzArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+fizzBuzzArray.forEach((n) => {
+ if (n % 3 === 0 && n % 5 === 0) console.log ("FizzBuzz")
+ else if (n % 3 === 0) console.log ("Fizz")
+ else if (n % 5 === 0) console.log ("Buzz")
+ else console.log (n)
+})
