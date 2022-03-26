82be7dd1ba1f7df8a0b1c789cce5bf7b0b772b52
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-03-25 20:12:02 -0400

    new update

--- js/filteredLists.js
+++ js/filteredLists.js
@@ -16,3 +16,26 @@ const numbers = [1, ...., 100]
 
 const numbers = Array(100).fill(0).map((e, i) => i + 1)
 console.log("numbers: ", numbers)
+const evenNumber = numbers.filter(n => n % 2 === 0)
+console.log ("Even Numbers : ",evenNumber )
+
+const oddNumber = numbers.filter(n => n % 2 === 1)
+
+console.log ("odd Numbers : ",oddNumber)
+/*
+const even_divisible = numbers.filter(n => n % 15 === 0)  && numbers.filter(n => n % 2 === 0)
+
+console.log ("Even Numbers and Numbers that are divisible by 15 : ",even_divisible)
+*/
+  
+const even_divisible = numbers.filter(n => n % 2 === 0 || n % 15 === 0)
+
+console.log ("Even Numbers and Numbers that are divisible by 15 : ",even_divisible)
+
+const betweenNumbers = numbers.filter(numbers => numbers >= 20 && numbers <= 30)
+
+console.log ("betweenNumbers 20 to 30 : ",betweenNumbers)
+
+const betweenNumbers2 = numbers.filter (numbers => numbers >= 10 && numbers <= 20 || numbers >= 30 && numbers <= 40)
+
+console.log ("betweenNumbers 10 to 40 : ",betweenNumbers2)
