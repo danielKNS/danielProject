3a25e6f7b14b568d7d9005cb6dcb43ac5a45096c
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-03-26 14:45:03 -0400

    Return

--- js/funtions.js
+++ js/funtions.js
@@ -17,7 +17,7 @@
 
 console.log("----- functions-------")
 
- function addNumbers(number1, number2 ) { // a function decloration 
+ function addNumbers(number1, number2 ) { // a function declaration 
      console.log("hello ")
      console.log("number 1 :",number1 )
      console.log("number 2 :", number2)
@@ -29,6 +29,21 @@ console.log("----- functions-------")
 addNumbers(100, 200)
 addNumbers(5, 6)
 addNumbers(3, 9)
+
+console.log("--- lucas es6 adding -----")
+
+const addNumbers2 = (number1, number2) => {
+    console.log("adding number1: ", number1, " to number2", number2)
+    return number1 + number2
+}
+const result1 = addNumbers2(100, 200)
+console.log("result1: ", result1)
+
+const result2 = addNumbers2(result1, 6)
+console.log("result2: ", result2)
+
+const result3 = addNumbers2(3, result2)
+console.log("result3: ", result3)
 
 console.log("-------my one------")
 
@@ -44,5 +59,8 @@ const subtractNumbers2 = (number1, number2) =>{
     console.log("helooooo")
     const total3 = number1 - number2
     console.log("total3 :", total3)
+    return total3
 }
-subtractNumbers2(2, 4)
+const total10 = subtractNumbers2(2, 4)
+console.log("total10 : ",total10)
+
