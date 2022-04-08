48689b391d0c1f83d9cb28e49c47f983d38ba158
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-04-07 20:44:23 -0400

    new project

--- js/react/hello-world-2/src/App.js
+++ js/react/hello-world-2/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className = "App">
+      <h1>What are my Favorite sport !!</h1>
+      <p>* VolleyBall</p>
+      <p>* Basketball</p>
+      <p>*FootBall</p>
+      <h1>The games that i would like to play </h1>
+      <p>*Elden Ring</p>
+      <p>*Ghost of tsushima</p>
+    </div> 
   );
 }
 
