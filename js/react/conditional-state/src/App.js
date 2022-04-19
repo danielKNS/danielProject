048a78e9abadbd2d8008b14b12ce241eeca93817
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-04-19 09:48:53 -0400

    pushing

---
+++ js/react/conditional-state/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css';
+// funtional component needs to return jxs and are allowed to use hooks
+// const App =() => {
+// const myName = "daniel"
+
+
+//  if (myName === "daniel") {
+//   return (
+//   <div>
+//     <h1> hi daniel </h1>
+//   </div>
+//   )
+//   } else {  
+//     return null
+//   }
+// }
+
+// return myName === "daniel" && (
+//   <div>
+//     <h1> hi daniel </h1>
+//   </div>
+// )
+
+// return (
+//   <div>
+//     <h1> hello there</h1>
+//     {myName === "daniel" && (
+//     <div>
+
+//     </div>
+//     )}
+//   </div>
+// )
+// }
+
+
+// class componenet return jxs your not allowed to use hooks
+
+export default App;
+
