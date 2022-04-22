ab4cf7a57c888940f57c7afbcb39a02daa1b2ecb
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-04-21 23:16:39 -0400

    finished FlipCoin

--- js/react/flip-Coin/src/game/FlipCoin.js
+++ js/react/flip-Coin/src/game/FlipCoin.js
@@ -10,7 +10,6 @@ return (
     <div>
         <div> 
             <h1>Flip Coin Game!!!</h1>
-            <h1 style={{color:"blue"}}> Choose: Head or Tails</h1>
         </div>
         <ImageCoin />
     </div>
--- js/react/flip-Coin/src/game/ImageCoin.js
+++ js/react/flip-Coin/src/game/ImageCoin.js
@@ -1,38 +1,41 @@
 import React, { useState } from "react"
 const ImageCoin = () => {
 
-    const [headMessage, SetHeadMessage] = useState("")
+    const [headMessage, SetHeadMessage, ] = useState("")
 
-
-    const handleCoinClick = (Coin) =>{
-        if (Coin === "Head"){
-            console.log("Head")
-            SetHeadMessage ("you choose Head")
+    const handleCoinClick = (Coin) =>{ // Coin is now Head
+        const randomNumber = Math.floor(Math.random() * 2)
+        console.log("randomNumber",randomNumber)
+        const result = randomNumber === 1 ? "Head" : "Tail"
+        console.log("result", result)
+        if (Coin === result){
+            SetHeadMessage(`YOU choose, ${Coin} , CPU picked ${result}, YOU WON!!`)
+        }else {
+            SetHeadMessage(`YOU choose, ${Coin} , CPU picked ${result}, YOU LOST!!`)
         }
-        else{
-            console.log("Tail")
-            SetHeadMessage("you choose Tail")
-        }
+       
     }
 
     return(
         <div className="intro">
-            <h2 style={{color:"yellow"}}> Let the game BEGIN!!</h2>
+            <h1 style={{color:"yellow"}}> Let the game BEGIN!!</h1>
+            <h2 style={{color:"blue"}}> Choose: Head or Tails :</h2>
             <div>
                 <button onClick={(Coin) => handleCoinClick("Head")}> {/* anonynous funtion */}
                     <img width={100} height={150} alt= "ImageCoin" src ="headImage.jpeg"></img> 
                 </button>
-                <button onClick={handleCoinClick}> 
+                <button onClick={(Coin) => handleCoinClick("Tail")}> 
                     <img width={100} height={150} alt= "ImageCoin" src ="tailImage.jpeg"></img> 
                 </button>
             </div>
             <div>
-                <p style={{color: "green"}}>{headMessage}</p>
-                
+                <p style= {{color: "yellow"}}>The results :</p>
+                <h2 style={{color: "green"}}>{headMessage} </h2>
             </div>
         </div>
         
     );
 }
+
 
 export default ImageCoin;
