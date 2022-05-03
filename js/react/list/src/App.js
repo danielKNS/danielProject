ff03b34c07c7d671d661f52ead7bcc7ae7058e50
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-05-03 18:44:32 -0400

    todo list

--- js/react/list/src/App.js
+++ js/react/list/src/App.js
@@ -7,57 +7,75 @@ const  App= ()  => {
       id: 1,
       text: "Take out trash and recycling",
       completed: true,
+      completeStatus:"todo complete"
       
     },
     {
       id: 2,
       text: "Pick up dry cleaning",
-      completed: false
+      completed: true,
+      completeStatus:"todo"
     },
     {
       id: 3,
       text: "Get oil change",
-      completed: false
+      completed: false,
+      completeStatus:"todo"
     },
     {
       id: 4,
       text: "Write thank-you notes",
-      completed: false
+      completed: false,
+      completeStatus:"todo"
     },
     {
     id: 5,
       text: "Take a walk",
-      completed: false
+      completed: true,
+      completeStatus:"todo"
     }
   ]);
   console.log("task",todoTask)
 
   //----------------------------------------------------
 
-  // i need to to something here where you know the guy that was clicked and the event to know whether it was CHEACKED or UNCHECKED.
-  //and then you need to be able to re-render state 
-
-  
-
+  // 
+  const [completetask,setCompletetask] = useState("")
   const handleClick = (task) => {
     console.log("i am being Clicked")
     console.log("Task Selected:",task)
     const todoStatus = task.completed
-    console.log("Todo is : ",todoStatus) //  show which task is being selected and is checking if the complete is true or False.
-  }
+    console.log("Todo is : ",todoStatus)//  show which task is being selected and is checking if the complete is true or False
+    const todo = todoTask 
+    todo.forEach(todo => {
+      if (todo.completed === true){
+        setCompletetask(todo.completeStatus = "todo complete")
+      
+      }else if(todo.completed === false) {
+        return setCompletetask (todo.completeStatus = "todo")
+
+      }
+    })
   
+  
+}
+
+// i having a problem of crossing the line everytime the checked box is being clicked.
+// i tried using if staments but i think i am missing something.
+// i am using className but is not working.
+// i the problem is from .map(line 54)
+
   return (
   <div>
 
       <div className="app"/>
         <h1>Things to Do : </h1>
         {todoTask.map((task) => <div id="main-todo-list"  className="todo-list" key={task.id}
-        onClick={()=> handleClick(task)}      // i just want to know which task is being selected. Also the "task" is each individual of the objects.
+        onClick={()=> handleClick(task)}     // i just want to know which task is being selected. Also the "task" is each individual of the objects.
         >
-        <div className="todo">
-          <input type="checkbox" className="todo-checkbox"/>
-          <span className="todo-text"> {task.text} </span>
-          <div ></div>
+        <div className={task.completeStatus}>
+          <input type="checkbox"  className="todo-checkbox" />
+          <span className="todo-text"> {task.text}  </span>
         </div>
           </div>
         )}
@@ -74,3 +92,4 @@ const  App= ()  => {
   // on line 53 i am mapping through all the task(array of object) 
 
   // the first need to be checked completed 
+  //.map .filter .find
