370d63d060f24319a119042bbfdf2a6d79ee6dca
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-05-04 22:50:25 -0400

    almost finishing the todo app

--- js/react/list/src/App.js
+++ js/react/list/src/App.js
@@ -13,7 +13,7 @@ const  App= ()  => {
     {
       id: 2,
       text: "Pick up dry cleaning",
-      completed: true,
+      completed: false,
       completeStatus:"todo"
     },
     {
@@ -31,43 +31,63 @@ const  App= ()  => {
     {
       id: 5,
       text: "Take a walk",
-      completed: true,
+      completed: false,
       completeStatus:"todo"
     }
-  ]);
-  console.log("task",todoTask)
-
-  //----------------------------------------------------
-
-  //  use the id  .map .filter .id .find
+  ]); 
+  // const event = { // look back in objects 
+  //   target: {
+  //     value:" hi "
+  //   }
+  // }
+  // 
+  //----------------------------------------------------//
+  const [testId,setTestId] = useState("")
   const [completetask,setCompletetask] = useState("") // is a hook fuction from react to create state varible
-  const handleClick = (task) => {
-    console.log("i am being Clicked")
+  const [taskRemain,setTaskRemain] = useState(todoTask.length)
+  const handleClick = (task,e) => {
+    console.log("e",e)
     console.log("Task Selected:",task)
     const todoStatus = task.completed
-    console.log("Todo is : ",todoStatus)//  show which task is being selected and is checking if the complete is true or False
-    const todo = todoTask 
-    todo.forEach(todo => {
-      if (todo.completed === true){
-              setCompletetask(todo.completeStatus = "todo complete")
-        }else if(todo.completed === false) {
-
-          return setCompletetask (todo.completeStatus = "todo")
-
+    console.log("Todo is : ",todoStatus)//  show which task is being selected and is checking if the complete is true or False 
+    if (todoStatus === true){
+      const newTask = todoTask.map(todo => {
+        if (todo.id === task.id){
+        return {...setCompletetask(todo, todo.completed = false ,todo.completeStatus = "todo")}
       }
-    })
+      return todo
+    });
+    setTestId(newTask)
+
+    const taskRemaining = newTask.filter(todo => todo.completed === false).length 
+    console.log("Tasks Remaining :",taskRemaining)
+    setTaskRemain(taskRemaining)
+
+  }else if (todoStatus === false){
+    const newTask = todoTask.map(todo => {
+      if (todo.id === task.id){
+      return{...setCompletetask(todo, todo.completed = true ,todo.completeStatus = "todo complete")}
+
+    }
+    return todo
+  });
+  setTestId(newTask)
   
+  const taskRemaining = newTask.filter(todo => todo.completed === false).length
+  console.log("Tasks Remaining :",taskRemaining)
+  setTaskRemain(taskRemaining)
+}
   
 }
-//
+// fix remain, allow to add more task, do the checkbox and finally remove the ones that are done.
 
  return (
   <div>
 
       <div className="app"/>
         <h1>Things to Do : </h1>
-        {todoTask.map((task) => <div id="main-todo-list"  className="todo-list" key={task.id}
-        onClick={()=> handleClick(task)}     // i just want to know which task is being selected. Also the "task" is each individual of the objects.
+        {todoTask.map((task) => <div id="main-todo-list"  className="todo-list"
+        key={task.id} onClick={(e)=> handleClick (task,e)}     // i just want to know which task is being selected. Also the "task" is each individual of the objects.
         >
         <div className={task.completeStatus}>
           <input type="checkbox"  className="todo-checkbox" />
@@ -84,7 +104,7 @@ const  App= ()  => {
   
   export default App;
   
-
+//  use the id  .map .filter .id .find
   // on line 53 i am mapping through all the task(array of object) 
 
   // the first need to be checked completed 
