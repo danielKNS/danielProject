8721c484369573dda1e41d788377ad555b1f4ff3
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-05-12 19:27:46 -0400

    instaApp

--- js/react/instagram/src/App.js
+++ js/react/instagram/src/App.js
@@ -5,16 +5,16 @@ import "./App.css";
 const  App= () => {
   const [posts, setPosts] = useState([
     {
+      id:1,
       username: "blessingthebobo",
       caption: "why is he looking at me ",
-      imageUrl:
-        "meme.jpeg",
+      imageUrl:"meme.jpeg",
     },
     {
+      id:2,
       username: "Jason",
-      caption: "Oh, I'm a God!, Season 3 is coming!!!!",
-      imageUrl:
-        "demonslayer.jpeg",
+      caption: "Oh my a God!, Season 3 is coming!!!!",
+      imageUrl:"demonslayer.jpeg",
     },
   ]);
   return (
@@ -22,8 +22,8 @@ const  App= () => {
       <div className="app__header">
         <div className="app__headerWrapper">
           <img
-            src=" https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-            alt="Instagram original logo"
+            width={90} height={90} src = "Instagram-Logo.png"
+            
           />
           <div className="app__headerButtons">
             <button className="primary__button">Log in</button>
