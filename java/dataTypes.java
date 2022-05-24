5fd403e9096a3280f5e755a6141acf51705612cd
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-05-23 21:13:07 -0400

    java

---
+++ java/dataTypes.java
@@ -0,0 +1,13 @@
+// everything must be inside a class.
+// understand to the best capabilities what all these words in the method means ?
+
+class DataTypes{
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!!!!!");
+
+        // how to create a synout all the main data types we learned in TS
+        // string , number , booleans, null, undefined,array,objects
+    }
+
+}
