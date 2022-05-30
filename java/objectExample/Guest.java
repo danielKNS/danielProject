f217285f0e78648cc07c9e0cd1b2f2b36ceed096
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-05-30 10:36:43 -0400

    practicing objects

--- java/objectExample/Guest.java
+++ java/objectExample/Guest.java
@@ -4,6 +4,11 @@ public class Guest {
     String firstName;
     String lastName;
     Integer age;
+    Integer year;
+    Integer favoriteNumber;
+    String favoriteFood;
+    Boolean isHappy;
+    Boolean doYouLikeAnime;
 
     // Constructor = creates an Instance of guest class
     // default Constructor
@@ -13,14 +18,43 @@ public class Guest {
 
     // all args Constructor
     public Guest(String firstName, String lastName, Integer age){
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+
     }
+
+    public Guest(String firstName, String lastName, Integer age,
+    Integer year, Integer favoriteNumber,String favoriteFood,
+    Boolean isHappy,Boolean doYouLikeAnime){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.year = year;
+        this.favoriteNumber = favoriteNumber;
+        this.favoriteFood = favoriteFood;
+        this.isHappy = isHappy;
+        this.doYouLikeAnime = doYouLikeAnime;
+    }
+
 
     //Methods = actions
     public void eat(String food){
         System.out.println(this.firstName + " is eating: "+ food);
+    }
+
+    public void favoriteAnime(String anime){
+        System.out.println(this.firstName + " Favorite anime is: "+ anime);
+    }
+
+    public void fullName(String middleName){
+        System.out.println("This Guest full name is: " + this.firstName +" "+ middleName +" "+ this.lastName );
+    }
+
+    public void guestPassword(Integer password){
+        System.out.println(this.firstName +" Passord is: "+ password);
     }
 }
  
--- java/objectExample/Party.java
+++ java/objectExample/Party.java
@@ -32,13 +32,30 @@ public class Party {
         System.out.println("------ All ARGS CONSTRUTOR --------");
         Guest guest3 = new Guest("Victoria","Portela",24);
 
-        System.out.println("guest2 "+guest3);
+        System.out.println("guest2 "+ guest3);
         System.out.println("guest3 fistName: "+ guest3.firstName);
         System.out.println("guest3 lastName: "+ guest3.lastName);
         System.out.println("guest3  age: "+ guest3.age);
-        guest1.eat("Nothing.... because she has HW to do ");
+        guest3.eat("Nothing.... because she has HW to do ");
 
-        List<Guest> guests = List.of(guest1,guest2,guest3);
+        System.out.println("------ All ARGS CONSTRUTOR TEST --------");
+        Guest guest4 = new Guest("Carlos","Santos",17,2002,5,"Mashed potatoes",false,true);
+
+        guest4.fullName("Rocha");
+        System.out.println("guest2 "+ guest4);
+        System.out.println("guest3 fistName: "+ guest4.firstName);
+        System.out.println("guest3 lastName: "+ guest4.lastName);
+        System.out.println("guest3  age: "+ guest4.age);
+        System.out.println("guest3 year you are born: " + guest4.year);
+        System.out.println("guest3 Favorite number: " + guest4.favoriteNumber);
+        System.out.println("guest3 Favorite food: " + guest4.favoriteFood);
+        System.out.println("Is guest3 happy: " + guest4.isHappy);
+        System.out.println("Does guest3 like Anime: " + guest4.doYouLikeAnime);
+        guest4.favoriteAnime("Sword art online");
+        guest4.eat("Nothing.... because he has coding to do ");
+        guest4.guestPassword(123456789);
+
+        List<Guest> guests = List.of(guest1,guest2,guest3,guest4);
         guests.forEach(guest -> {
             System.out.println("The following guest has just walked in..."+ guest.firstName + " " + guest.lastName);
         });
