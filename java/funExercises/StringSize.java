d671656c1f90ad56c9084988a429090e53ba083e
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-06-13 15:37:28 -0400

    finished String task

---
+++ java/funExercises/StringSize.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StringSize {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("what is your word ? ");
+
+       String userInput = scanner.nextLine();
+       System.out.println("hello " + userInput);
+
+       Integer charater = 0;
+
+
+       for(Integer c = 0; c < userInput.length(); c++){
+           if(userInput.charAt(c)!= ' ') // avoiding counting space.
+           charater++; // increase the count of the characters in the given string.
+       }
+       System.out.println("hello " + charater);
+    }
+}
