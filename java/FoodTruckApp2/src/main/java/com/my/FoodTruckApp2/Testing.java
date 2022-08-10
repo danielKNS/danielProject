f3df386d9f531a89044dcc4a888c3c50259fcf49
Daniel Ribeiro <danielrocha05lol@gmail.com>
2022-08-10 19:20:52 -0400

    making sure that i saved my work

--- java/FoodTruckApp2/src/main/java/com/my/FoodTruckApp2/Testing.java
+++ java/FoodTruckApp2/src/main/java/com/my/FoodTruckApp2/Testing.java
@@ -1,8 +1,31 @@
 package com.my.FoodTruckApp2;
-//package net.javaguides.postgresql.tutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class Testing {
+// DriverManager(actually does the connection to the database)
+// use annotations to connect files to the main app, it cannot be stand alone app. it needs to connect to spring
+// theres a bunch of different ways to do it.
+// should not have main method you should not have multiples mains
+// todo: press the play buttom of the main app, it will do it for you not some random file
+public class Testing implements CommandLineRunner {
+//    String url = "jdbc:postgresql://localhost/test_dummy";
+//    String user = "owner";
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Override
+    public void run(String... args) throws Exception {
+        String sql = "INSERT INTO customer VALUES (5,'Carlos','Neto')";
+
+        Integer rows = jdbcTemplate.update(sql);
+        if(rows > 0){
+            System.out.println("A new row has been inserted!!!");
+        }
+
+    }
 }
