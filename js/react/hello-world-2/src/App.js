69c731a73a7902eda35c3f0308e2b97cfba227e0
Daniel Ribeiro <danielrocha05lol@gmail.com>
2026-01-18 16:13:05 -0500

    o jogo

--- js/react/hello-world-2/src/App.js
+++ js/react/hello-world-2/src/App.js
@@ -8,6 +8,7 @@ const  App = () => {
       <p>* VolleyBall</p>
       <p>* Basketball</p>
       <p>*FootBall</p>
+      <h1>O JOGO</h1>
       <h1>The games that i would like to play </h1>
       <p>*Elden Ring</p>
       <p>*Ghost of tsushima</p>
